fe74dd7908aaa03f92b9fa6f2f4e8a5ba560863d
Jackson <sealj553@gmail.com>
2018-02-09 16:08:05 -0500

    removed unnecessary if

--- dmenu-aliases-zsh.sh
+++ dmenu-aliases-zsh.sh
@@ -5,10 +5,9 @@ aliases_file=~/.zsh_aliases
 
 #parse and sort aliases file, sort command list, prepend aliases to commands, pass to dmenu
 cmd=$({cat $aliases_file | sed '/^#/ d;/^$/d;s/^alias\s//g;s/=/ /' | sort & print -rl ${(k)commands} | sort;} | eval "$dmenu_params")
-if [ ! -z $cmd ]; then
-    #if the second word starts with " it is assumed to be an alias - remove first word and remove all "s
-    if [ "$(print $cmd | awk '{print $2}' | head -c 1)" = "\"" ]; then
-        cmd=$(print $cmd | cut -d' ' -f2- | tr -d "\"") 
-    fi
-    eval "$cmd"
+
+#if the second word starts with " it is assumed to be an alias - remove first word and remove all "s
+if [ "$(print $cmd | awk '{print $2}' | head -c 1)" = "\"" ]; then
+    cmd=$(print $cmd | cut -d' ' -f2- | tr -d "\"") 
 fi
+eval "$cmd"
